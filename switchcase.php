02337126ab95f7aa24ebd0910b541753eb16a4ba
manifestdev <mohantypratik257@gmail.com>
2025-05-07 20:07:50 +0530

    switchcase

---
+++ switchcase.php
@@ -0,0 +1,17 @@
+<?php
+$fruit = "apple";
+
+switch ($fruit) {
+    case "apple":
+        echo "This is an apple.";
+        break;
+    case "banana":
+        echo "This is a banana.";
+        break;
+    case "orange":
+        echo "This is an orange.";
+        break;
+    default:
+        echo "Unknown fruit.";
+}
+?>
