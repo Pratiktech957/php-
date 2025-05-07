03122b915a57ff8a45ddeb15a108d4d53a6a04d1
manifestdev <mohantypratik257@gmail.com>
2025-05-07 20:17:02 +0530

    whileloops

---
+++ whileloops.php
@@ -0,0 +1,38 @@
+<?php
+
+
+$num = 10;
+do{
+    echo "the number is $num <br>";
+    $num++;
+}
+while($num<10);
+
+
+
+?>
+
+<?php
+
+$num = 10;
+$sum = 0;
+for($i=0;$i<=$num;$i++){
+    $sum+=$i;
+
+}
+
+echo "the sum of the number is $sum";
+
+
+
+?>
+
+<?php
+$counter = 5;
+while ($counter >= 1) {
+    echo "Countdown: $counter<br>";
+    $counter--;
+}
+?>
+
+
